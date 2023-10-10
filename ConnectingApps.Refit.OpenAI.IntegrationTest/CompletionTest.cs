052a520e6b75a1bc86aec3cdb6c9ac23fd691021
Daan Acohen <daan.acohen@connectingapps.net>
2023-10-10 12:16:57 +0200

    Created the first test

---
+++ ConnectingApps.Refit.OpenAI.IntegrationTest/CompletionTest.cs
@@ -0,0 +1,41 @@
+using System.Net;
+using ConnectingApps.Refit.OpenAI.Completions;
+using ConnectingApps.Refit.OpenAI.Completions.Request;
+using FluentAssertions;
+using Refit;
+using ConnectingApps.Refit.OpenAI.Completions.Response;
+
+namespace ConnectingApps.Refit.OpenAI.IntegrationTest
+{
+    public class CompletionTest
+    {
+        private static readonly Func<ChatRequest, Task<ApiResponse<ChatResponse>>> CompletionCaller;
+
+        static CompletionTest()
+        {
+            var apiKey = Environment.GetEnvironmentVariable("OPENAI_KEY");
+            apiKey.Should().NotBeNullOrEmpty("OPENAI_KEY environment variable must be set");
+            var completionApi = RestService.For<ICompletion>("https://api.openai.com", OpenAiRefitSettings.RefitSettings);
+            CompletionCaller = chatRequest => completionApi.CreateCompletionAsync(chatRequest, $"Bearer {apiKey}");
+        }
+
+        [Fact]
+        public async Task Test1()
+        {
+            var response = await CompletionCaller(new ChatRequest
+            {
+                Model = "gpt-3.5-turbo",
+                Temperature = 0.7,
+                Messages = new List<Completions.Request.Message>
+                {
+                    new()
+                    {
+                        Role = "user",
+                        Content = "What is the capital of the France?",
+                    }
+                }
+            });
+            (response.Error?.Content, response.StatusCode).Should().Be((null, HttpStatusCode.OK));
+        }
+    }
+}
