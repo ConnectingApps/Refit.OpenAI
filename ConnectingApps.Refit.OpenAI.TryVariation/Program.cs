402af8a418fcb78e1fabef8756737afdc05cefec
Daan Acohen <daan.acohen@connectingapps.net>
2023-10-11 14:43:01 +0200

    Successfull variation detection in a command-line application

--- ConnectingApps.Refit.OpenAI.TryVariation/Program.cs
+++ ConnectingApps.Refit.OpenAI.TryVariation/Program.cs
@@ -1,1 +1,29 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using ConnectingApps.Refit.OpenAI;
+using ConnectingApps.Refit.OpenAI.Variations;
+using Refit;
+
+Console.WriteLine("Hello, World!");
+
+var apiKey = Environment.GetEnvironmentVariable("OPENAI_KEY");
+var authorizationHeader = $"Bearer {apiKey}";
+var image = new FileStream("otter.png", FileMode.Open, FileAccess.Read);
+
+var openAiApi = RestService.For<IVariationApi>("https://api.openai.com", OpenAiRefitSettings.RefitSettings);
+
+try
+{
+    var response = await openAiApi.GenerateImageVariations(authorizationHeader, new GenerateImageVariationsRequest()
+    {
+        N = 2,
+        Size = "1024x1024",
+        Image = image
+    });
+    Console.WriteLine($"Returned response status code {response.StatusCode}");
+}
+catch (Exception e)
+{
+    Console.WriteLine(e);
+    throw;
+}
+
+
---
+++ ConnectingApps.Refit.OpenAI/Variations/IVariationApi.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using Refit;
+
+namespace ConnectingApps.Refit.OpenAI.Variations
+{
+    public interface IVariationApi
+    {
+        [Post("/v1/images/variations")]
+        Task<ApiResponse<GenerateImageVariationsResponse>> GenerateImageVariations(
+            [Header("Authorization")] string authorizationHeader,
+            [Body] GenerateImageVariationsRequest request);
+    }
+
+    public class GenerateImageVariationsRequest
+    {
+        public Stream Image { get; set; }
+        public int N { get; set; }
+        public string Size { get; set; }
+    }
+
+    public class GenerateImageVariationsResponse
+    {
+        public long Created { get; set; }
+        public List<GenerateImageVariationResponse> Data { get; set; }
+    }
+
+    public class GenerateImageVariationResponse
+    {
+        public string Url { get; set; }
+    }
+
+}
