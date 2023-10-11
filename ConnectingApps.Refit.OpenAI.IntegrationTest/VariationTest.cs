c6d920ea65f31487edcb8a6db02f18518149d09b
Daan Acohen <daan.acohen@connectingapps.net>
2023-10-11 15:41:39 +0200

    Created a test for calling the variation API

---
+++ ConnectingApps.Refit.OpenAI.IntegrationTest/VariationTest.cs
@@ -0,0 +1,33 @@
+﻿using ConnectingApps.Refit.OpenAI.Variations;
+using FluentAssertions;
+using Refit;
+using System.Net;
+
+namespace ConnectingApps.Refit.OpenAI.IntegrationTest
+{
+    public class VariationTest
+    {
+        private readonly string _authorizationHeader;
+        private readonly IVariation _variationApi;
+
+        public VariationTest() 
+        {
+            var apiKey = Environment.GetEnvironmentVariable("OPENAI_KEY");
+            apiKey.Should().NotBeNullOrEmpty("OPENAI_KEY environment variable must be set");
+            _authorizationHeader = $"Bearer {apiKey}";
+            _variationApi = RestService.For<IVariation>("https://api.openai.com", OpenAiRefitSettings.RefitSettings);
+        }
+
+        [Fact]
+        public async Task CallVariation()
+        {
+            await using (var image = new FileStream("otter.png", FileMode.Open, FileAccess.Read))
+            {
+                var streamPart = new StreamPart(image, "otter.png");
+                var response = await _variationApi.GetImageVariations(_authorizationHeader, streamPart, 2, "1024x1024");
+                (response.Error?.Content, response.StatusCode).Should().Be((null, HttpStatusCode.OK));
+                response.Content!.Data.Should().HaveCount(2);
+            }
+        }
+    }
+}
