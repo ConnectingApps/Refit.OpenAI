e6c8cced67849a202a1ac55893856af6d7ef80fb
Daan Acohen <daan.acohen@connectingapps.net>
2023-10-11 14:56:42 +0200

    Some small refactoring

--- ConnectingApps.Refit.OpenAI.TryVariation/Program.cs
+++ ConnectingApps.Refit.OpenAI.TryVariation/Program.cs
@@ -2,28 +2,17 @@
 using ConnectingApps.Refit.OpenAI.Variations;
 using Refit;
 
-Console.WriteLine("Hello, World!");
-
 var apiKey = Environment.GetEnvironmentVariable("OPENAI_KEY");
 var authorizationHeader = $"Bearer {apiKey}";
-var image = new FileStream("otter.png", FileMode.Open, FileAccess.Read);
-
-var openAiApi = RestService.For<IVariationApi>("https://api.openai.com", OpenAiRefitSettings.RefitSettings);
-
-var exist = File.Exists("otter.png");
-Console.WriteLine($"File exists: {exist}");
-var streamPart = new StreamPart(image, "otter.png");
-
-try
+await using (var image = new FileStream("otter.png", FileMode.Open, FileAccess.Read))
 {
+    var openAiApi = RestService.For<IVariationApi>("https://api.openai.com", OpenAiRefitSettings.RefitSettings);
+    var exist = File.Exists("otter.png");
+    Console.WriteLine($"File exists: {exist}");
+    var streamPart = new StreamPart(image, "otter.png");
     var response = await openAiApi.GetImageVariations(authorizationHeader, streamPart, 2, "1024x1024");
-
     Console.WriteLine($"Returned response status code {response.StatusCode}");
+    Console.WriteLine($"Number of new items created {response.Content!.Data.Count}");
+    Console.WriteLine($"First item url {response.Content!.Data.First().Url}");
+    Console.WriteLine($"Second item url {response.Content!.Data.Last().Url}");
 }
-catch (Exception e)
-{
-    Console.WriteLine(e);
-    throw;
-}
-
-
