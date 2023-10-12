c46aab008a8551cc85a320c46f68b483c1e6e167
Daan Acohen <daan.acohen@connectingapps.net>
2023-10-12 16:34:12 +0200

    Refactoring

--- ConnectingApps.Refit.OpenAI.TryAudioTranslate/Program.cs
+++ ConnectingApps.Refit.OpenAI.TryAudioTranslate/Program.cs
@@ -4,10 +4,10 @@ using Refit;
 
 var apiKey = Environment.GetEnvironmentVariable("OPENAI_KEY");
 var authorizationHeader = $"Bearer {apiKey}";
-await using (var image = new FileStream("HalloWereld.mp3", FileMode.Open, FileAccess.Read))
+await using (var recording = new FileStream("HalloWereld.mp3", FileMode.Open, FileAccess.Read))
 {
     var openAiApi = RestService.For<IAudioTranslation>("https://api.openai.com", OpenAiRefitSettings.RefitSettings);
-    var streamPart = new StreamPart(image, "HalloWereld.mp3");
+    var streamPart = new StreamPart(recording, "HalloWereld.mp3");
     var response = await openAiApi.GetAudioTranslation(authorizationHeader, streamPart, "whisper-1");
     Console.WriteLine($"Returned response status code {response.StatusCode}");
     Console.WriteLine($"Translated text {response.Content!.Text}");
