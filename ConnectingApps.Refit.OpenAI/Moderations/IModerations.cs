4685448d4e4503b381a3c715a9eed7df900af817
Daan Acohen <daan.acohen@connectingapps.net>
2023-10-13 17:44:02 +0200

    Added a Refit interface for moderation

---
+++ ConnectingApps.Refit.OpenAI/Moderations/IModerations.cs
@@ -0,0 +1,14 @@
+﻿using Refit;
+using System.Threading.Tasks;
+using ConnectingApps.Refit.OpenAI.Moderations.Response;
+using ConnectingApps.Refit.OpenAI.Moderations.Request;
+
+namespace ConnectingApps.Refit.OpenAI.Moderations
+{
+    public interface IModeration
+    {
+        [Post("/v1/moderations")]
+        Task<ApiResponse<ModerationsResponse>> CreateCompletionAsync([Body] ModerationRequest chatRequest,
+            [Header("Authorization")] string authorization);
+    }
+}
