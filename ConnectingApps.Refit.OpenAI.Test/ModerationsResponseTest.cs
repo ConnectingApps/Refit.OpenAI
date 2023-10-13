851a14cd64831c7d9c77ac7068e8226b8b8302d8
Daan Acohen <daan.acohen@connectingapps.net>
2023-10-13 17:32:15 +0200

    Added more tests to test serialization

--- ConnectingApps.Refit.OpenAI.Test/ModerationsResponseTest.cs
+++ ConnectingApps.Refit.OpenAI.Test/ModerationsResponseTest.cs
@@ -104,6 +104,43 @@ namespace ConnectingApps.Refit.OpenAI.Test
                       ]
                     }
                     """)]
+        [InlineData("""
+                    {
+                      "id": "modr-89CCHcVvSKTnHSuF8VRtmMXLfBtcA",
+                      "model": "text-moderation-006",
+                      "results": [
+                        {
+                          "flagged": true,
+                          "categories": {
+                            "sexual": true,
+                            "hate": true,
+                            "harassment": true,
+                            "self-harm": true,
+                            "sexual/minors": true,
+                            "hate/threatening": true,
+                            "violence/graphic": true,
+                            "self-harm/intent": true,
+                            "self-harm/instructions": true,
+                            "harassment/threatening": true,
+                            "violence": true
+                          },
+                          "category_scores": {
+                            "sexual": 3.6963891034247354e-05,
+                            "hate": 0.0001245409803232178,
+                            "harassment": 0.009186690673232079,
+                            "self-harm": 9.155368752544746e-05,
+                            "sexual/minors": 2.1913865566602908e-07,
+                            "hate/threatening": 1.2790572327503469e-05,
+                            "violence/graphic": 4.758815703098662e-05,
+                            "self-harm/intent": 8.181822340702638e-05,
+                            "self-harm/instructions": 2.0860365518293733e-11,
+                            "harassment/threatening": 0.013410148210823536,
+                            "violence": 0.9223754405975342
+                          }
+                        }
+                      ]
+                    }
+                    """)]
         public void TestDeSerializeAndDeserializeModerationsResponse(string jsonString)
         {
             var jsonOptions = new JsonSerializerOptions
