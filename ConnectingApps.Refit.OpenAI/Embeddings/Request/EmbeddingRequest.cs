bc4185c08ca3a925cfcbf7126d10ea69898b6c1a
Daan Acohen <daan.acohen@connectingapps.net>
2023-10-16 21:53:07 +0200

    Embedding interface added

---
+++ ConnectingApps.Refit.OpenAI/Embeddings/Request/EmbeddingRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace ConnectingApps.Refit.OpenAI.Embeddings.Request
+{
+    public class EmbeddingRequest
+    {
+        public string Input { get; set; } = null!;
+        public string Model { get; set; } = null!;
+    }
+}
