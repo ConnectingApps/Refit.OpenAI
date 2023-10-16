32425352953ded7e30ff9bc8face1542fcf4512f
Daan Acohen <daan.acohen@connectingapps.net>
2023-10-16 13:50:09 +0200

    File management try out project created

---
+++ ConnectingApps.Refit.OpenAI.TryFileManagement/Program.cs
@@ -0,0 +1,15 @@
+﻿using ConnectingApps.Refit.OpenAI;
+using ConnectingApps.Refit.OpenAI.Files;
+using Refit;
+
+
+var apiKey = Environment.GetEnvironmentVariable("OPENAI_KEY");
+var completionApi = RestService.For<IFiles>(new HttpClient
+{
+    BaseAddress = new Uri("https://api.openai.com")
+}, OpenAiRefitSettings.RefitSettings);
+
+var getResponse = await completionApi.GetFilesAsync( $"Bearer {apiKey}");
+
+Console.WriteLine($"Returned GET response status code {getResponse.StatusCode}");
+Console.WriteLine($"Number of items {getResponse.Content!.Data.Length}");
