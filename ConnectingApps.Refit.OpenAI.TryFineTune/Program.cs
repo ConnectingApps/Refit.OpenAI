fa0bb8f6740780a7dcfe36455f5d525a379965dd
Daan Acohen <daan.acohen@connectingapps.net>
2023-10-24 19:56:14 +0200

    First tryout of new jobs

---
+++ ConnectingApps.Refit.OpenAI.TryFineTune/Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ConnectingApps.Refit.OpenAI;
+using ConnectingApps.Refit.OpenAI.Files;
+using ConnectingApps.Refit.OpenAI.FineTune;
+using ConnectingApps.Refit.OpenAI.FineTune.Request;
+using Refit;
+
+Console.WriteLine("Hello, World!");
+
+var apiKey = Environment.GetEnvironmentVariable("OPENAI_KEY");
+var fineTuneApi = RestService.For<IFineTune>(new HttpClient
+{
+    BaseAddress = new Uri("https://api.openai.com")
+}, OpenAiRefitSettings.RefitSettings);
+var token = $"Bearer {apiKey}";
+
+var jobs = await fineTuneApi.GetJobsAsync(token);
+
+Console.WriteLine($"Returned response status code {jobs.StatusCode}");
+Console.WriteLine($"Number of jobs {jobs.Content!.Data.Length}");
+string newTraingFile = null!;
+
+await using (var image = new FileStream("mydata.jsonl", FileMode.Open, FileAccess.Read))
+{
+    var openAiApi = RestService.For<IFiles>("https://api.openai.com", OpenAiRefitSettings.RefitSettings);
+    var streamPart = new StreamPart(image, "mydata.jsonl");
+    var postFileResponse = await openAiApi.PostFileAsync(token, streamPart, "fine-tune");
+    Console.WriteLine($"Returned POST response status code {postFileResponse.StatusCode}");
+    Console.WriteLine($"Returned POST response number of bytes {postFileResponse.Content!.Bytes}");
+    newTraingFile = postFileResponse.Content!.Id;
+}
+
+var newJobResponse = await fineTuneApi.PostJob(new FineTuneRequest
+{
+    TrainingFile = newTraingFile,
+    Model = "gpt-3.5-turbo"
+}, token);
+
+Console.WriteLine($"New job response {newJobResponse.StatusCode}");
+
+var newJobs = await fineTuneApi.GetJobsAsync(token);
+Console.WriteLine($"New Job files Returned response status code {newJobs.StatusCode}");
+Console.WriteLine($"Number of jobs after POST {newJobs.Content!.Data.Length}");
